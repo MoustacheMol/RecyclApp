dd3cb826fcbfd7314593271023989fef98f16a02
PMarcL <pmleva@hotmail.com>
2014-10-29 20:38:00 -0400

    ajout des méthodes à compléter

--- src/recyclapp/controller/RecyclAppViewController.java
+++ src/recyclapp/controller/RecyclAppViewController.java
@@ -1,10 +1,42 @@
 package recyclapp.controller;
 
+import recyclapp.domain.CentreDeTri;
+import recyclapp.domain.Equipement;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  *
  * @author Pierre-Marc Levasseur <pierre-marc.levasseur.1@ulaval.ca>
  */
 public class RecyclAppViewController
 {
+    CentreDeTri centreDeTri;
+    Equipement selectionCourrante;
     
+    public Dimension getDimensionCentreDeTri()
+    {
+        return centreDeTri.getDimensions();
+    }
+    
+    public ArrayList<Equipement> getEquipements()
+    {
+        return centreDeTri.getListEquipement();
+    }
+    
+    public void ajouterConvoyeur()
+    {
+        
+    }
+    
+    public boolean ajouterEquipement(Point p, Dimension d)
+    {
+        return true;
+    }
+    
+    public void clickUtilisateur(Point p)
+    {
+        
+    }
 }
